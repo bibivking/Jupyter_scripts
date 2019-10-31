f2febfd1cadb480f76ed380afce1472193eaa2b5
Anne Mu <bibi_king@163.com>
2019-11-01 00:12:28 +1100

    test performance

--- parameters.py
+++ parameters.py
@@ -1,45 +1,36 @@
 """
 Model parameters
-
-That's all folks.
 """
 
 import math
 import numpy as np
 
-
 # gravity (m s-2) 
-g = 9.8086
+g  = 9.8086
 
-Pi        = math.pi
-
-
-Rwv       = *        # gas constand for water vapor
+Pi = math.pi
     
-rho       = 1000.
+rho = 1000.
 
 '''
 soil hydraulic parameters
 '''
+# mm/s
+hyds = 0.023635
 
-hyds =
-
-bch  = 
-
-#ssat, saturated water content
-theta_sat = 
-
-#watr, residual water content
-theta_r   = 
+bch  = 5.537291
 
 # abs(soil%sucs_vec(:,1)/1000. (m) the soil matric potential at saturation 
-Phi_sat   =         
+sucs = 0.073613075
 
-# soil matric potential in the first soil layer
-Phi_1     =  
+#ssat, saturated water content
+theta_sat = 0.396073
 
+#watr, residual water content
+theta_r   = 0.02
 
-Rwv
+# Specific gas constant J/(kg·K) (wikipedia's value is 461.5)
+Rwv = 461.4
     
 # lm=1.73e-5, converts units
 gamma = 0.0000173 
@@ -48,7 +39,7 @@ gamma = 0.0000173
 a_3 = 1.25 
 
 # rough%hruff (m) canopy height for evergreen broadleaf
-h  = 35. 
+h  = 20. #35. 
 #rough%hruff = MAX( 1.e-6,veg%hc - 1.2*ssnow%snowd/MAX(ssnow%ssdnn,100.)), canopy height above snow level
     
 # C%CSW canopy sw decay (Weil theory), determine decrease rate of standard deviation of canopy vertical velocity
@@ -58,20 +49,14 @@ Csw = 0.50
 Ctl = 0.40 
 
 # (Jm-2) surface tension of liquid water 
-sigma     = 0.071 
+sigma = 0.071 
 
-    
 #  canopy%sublayer_dz, height above the soil surface of U_x
 z = 0.005
-        
-
-    
 
 # air%rlam (J kg-1) latent heat of vaporization 
-Lv     = 2501400. 
+Lv = 2501400. 
 # air%rlam= C%HL (J kg-1) 
-    
-p.rho = 
 
 # rt_Dff=2.5e-5 (m2 s-1), diffusion of wtare vapor
-p.D     = 0.0000282 
+D = 0.0000282 
